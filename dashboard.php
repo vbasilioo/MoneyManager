cf58b72cc83863410b02c2bd42250ff542b54d9c
shen <vbasilio2019@gmail.com>
2023-07-20 16:36:08 -0300

    feature: new navbar and methods balance add.
    Co-authored-by: Maria Clara Rocha <mariaclararcs@users.noreply.github.com>

--- dashboard.php
+++ dashboard.php
@@ -20,7 +20,7 @@ if(isset($_GET['logout'])){
 $userController = new UserController();
 
 $financeController = new Finance();
-$finances = $financeController->ShowFinances();
+$finances = $financeController->ShowFinances($_SESSION['user_id']);
 
 $accountController = new AccountController();
 $accounts = $accountController->ShowAccounts($_SESSION['user_id']);
@@ -30,8 +30,8 @@ if(isset($_POST['account_name']) && isset($_POST['account_value'])){
     header("Location: dashboard.php");
 }
 
-if(isset($_POST['accountPay'])){
-    $accountController->PaymentAccount($account['value'], $_POST['account_id']);
+if(isset($_POST['accountPay']) && isset($_POST['account_id'])){
+    $accountController->PaymentAccount($_POST['account_id']);
     header("Location: dashboard.php");
 }
 ?>
@@ -41,9 +41,30 @@ if(isset($_POST['accountPay'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>MoneyManager</title>
 </head>
-<body style="background-color: #80A56D;">
+<body>
+    <div class="container-fluid text-bg-dark">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-md-3 mb-2 mb-md-0">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                </a>
+            </div>
+
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-secondary">Início</a></li>
+                <li><a href="#" class="nav-link px-2">Contas</a></li>
+                <li><a href="#" class="nav-link px-2">Balanços</a></li>
+            </ul>
+
+            <div class="col-md-3 text-end">
+                <a href="?logout=true" style="text-decoration: none;">Sair</a>
+            </div>
+        </header>
+    </div>
+
     <h1>Balanço</h1>
     <ul>
         <?php foreach($finances as $finance): ?>
@@ -64,7 +85,9 @@ if(isset($_POST['accountPay'])){
             <?php echo $account['nameAccount'] . ' ' . 'R$' . $account['value']; ?>
             <?php if($account['accountPay'] == 0): ?>
                 <form method="POST" action="dashboard.php">
-                    <input type="hidden" name="account_id" value="<?php echo $account['ID']; ?>">
+                    <?php if(isset($account['ID'])): ?>
+                        <input type="hidden" name="account_id" value="<?php echo $account['ID']; ?>">
+                    <?php endif; ?>
                     <button type="submit" name="accountPay">Pagar conta</button>
                 </form>
             <?php else: ?>
@@ -73,6 +96,7 @@ if(isset($_POST['accountPay'])){
         </li>
         <?php endforeach; ?>
     </ul>
-    <a href="?logout=true">Sair</a>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
